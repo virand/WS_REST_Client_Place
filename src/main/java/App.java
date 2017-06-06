ff9c1eee614856b28724cdbd30c3a198612a61ad
virand <info@virand.ru>
2017-06-07 00:49:20 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,42 @@
+/**
+ * Created by virand on 6/6/2017.
+ */
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.GenericType;
+import com.sun.jersey.api.client.WebResource;
+
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+public class App {
+    private static final String URL ="http://localhost:8081/rest/places";
+
+    public static void main(String[] args){
+        Client client = Client.create();
+        printList(getPlaces(client, null));
+        System.out.println();
+        printList(getPlaces(client,"Исаакиевский собор"));
+    }
+
+    private static List<Place> getPlaces(Client client, String name){
+        WebResource webResource = client.resource(URL);
+        if(name != null){
+            webResource=webResource.queryParam("word",name);
+        }
+        ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+        if(response.getStatus()!=ClientResponse.Status.OK.getStatusCode()){
+            throw new IllegalStateException("Request failed");
+        }
+
+        GenericType<List<Place>>type = new GenericType<List<Place>>(){};
+        return response.getEntity(type);
+    }
+
+    private static void printList(List<Place> places)
+    {
+        for (Place place : places)
+            System.out.println(place);
+    }
+}
---
+++ src/main/java/Place.java
@@ -0,0 +1,69 @@
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Created by virand on 6/6/2017.
+ */
+
+
+    @XmlRootElement
+    public class Place {
+        private int id;
+        private String name;
+        private String description;
+        private String city;
+        private String address;
+
+        public Place() {
+
+        }
+
+        public Place(int aId, String aName, String aDescription, String aCity, String aAddress)
+        {
+            id=aId;
+            name=aName;
+            description=aDescription;
+            city=aCity;
+            address=aAddress;
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public String getDescription(){
+            return description;
+        }
+
+        public String getCity(){
+            return city;
+        }
+
+        public String getAddress()
+        {
+            return address;
+        }
+
+        public void setName(String aName){
+            name=aName;
+        }
+
+        public void setDescription(String aDescription)
+        {
+            description=aDescription;
+        }
+
+        public void setCity(String aCity)
+        {
+            city=aCity;
+        }
+
+        public void setAddress(String aAddress)
+        {
+            address=aAddress;
+        }
+
+        @Override
+        public String toString(){
+            return "Place{"+"name = " + name + " description = " + description + " city = " + city + " address = " + address;
+        }
+    }
